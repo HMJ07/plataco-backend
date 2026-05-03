6bafb85bc1f8816f92a59549f965057e3ae2ef4b
HMJ07 <106169726+HMJ07@users.noreply.github.com>
2026-05-03 20:29:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,4 +7,4 @@ window.PLATACO_API_URL = 'https://plataco-backend-production.up.railway.app/api'
 // Tu clave PÚBLICA de Stripe (empieza con pk_test_ o pk_live_)
 // Obtén la tuya en: https://dashboard.stripe.com/apikeys
 // ⚠️ Esta es la clave PÚBLICA, es seguro ponerla aquí
-window.PLATACO_STRIPE_PK = 'pk_test_PON_AQUI_TU_CLAVE_PUBLICA_DE_STRIPE';
+window.PLATACO_STRIPE_PK = 'pk_live_51TSdgCL5LZhGFOW1M6URpBhKuvhep687F24WA50sT170CPSnBmOjUpxBj9fZSHCY4lgr98ANS3bfr0qPTNopxEU400BTn9xs9L';
